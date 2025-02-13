aec1489820c4476228d86e1214f7b8e199f97bac
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-02-13 12:51:43 -0600

    Added Workout List

---
+++ src/components/WorkoutList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  workouts: Array,
+});
+
+const emit = defineEmits(['deleteWorkout', 'editWorkout']);
+</script>
+
+<template>
+  <h3>Workout History</h3>
+  <ul>
+    <li v-for="workout in workouts" :key="workout.id">
+      <div>
+        <strong>{{ workout.exercise }}</strong> - {{ workout.duration }} min, {{ workout.calories }} cal
+        <br />
+        Heart Rate: {{ workout.heartRate }} BPM | Reps: {{ workout.reps }} | Sets: {{ workout.sets }}
+      </div>
+      <button @click="emit('editWorkout', workout)">Edit</button>
+      <button @click="emit('deleteWorkout', workout.id)">Delete</button>
+    </li>
+  </ul>
+</template>
